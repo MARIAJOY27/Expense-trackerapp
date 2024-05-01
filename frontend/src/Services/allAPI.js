1cf5ac3da0bc71a97c26cd470c0f8e5cd3659cba
SNEHA-MKK <sneha.mohandas.kk@gmail.com>
2024-05-02 01:46:15 +0530

    git pull

--- frontend/src/Services/allAPI.js
+++ frontend/src/Services/allAPI.js
@@ -1,11 +1,21 @@
 
 
-//api for adding income
-
 import { serverURL } from "./baseUrl"
 import { commonAPI } from "./commonAPI"
 
+
+//api for adding income
  export const uploadIncomeAPI = async(reqBody) =>{
      return await commonAPI('POST',`${serverURL}/incomes`,reqBody)
      
+}
+
+//api to get uploaded incomes
+ export const getIncomeAPI = async()=>{
+     return await commonAPI('GET',`${serverURL}/incomes`,"")
+ }
+
+ // api to delete an income
+ export const deleteAnIncomeAPI= async(id)=>{
+    return await commonAPI('DELETE',`${serverURL}/incomes/${id}`,{})
 }
